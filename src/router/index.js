e45eb312a471d22733d191b1afa76bd9fb685601
Patrik Procházka <>
2020-06-21 16:03:16 +0200

    Comments and netlify fix

--- src/router/index.js
+++ src/router/index.js
@@ -1,66 +1,58 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
-import Test from '../views/Test.vue'
-import Vysledek from '../views/Vysledek.vue'
-import Organizace from '../views/Organizace.vue'
-import RegisterOrg from '../views/RegisterOrg.vue'
-import OrgCard from '../components/OrgCard.vue'
-import Projekt from '../views/Projekt.vue'
-import Exitus from '../views/Exitus.vue'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Home from "../views/Home.vue";
+import Test from "../views/Test.vue";
+import Vysledek from "../views/Vysledek.vue";
+import Organizace from "../views/Organizace.vue";
+import RegisterOrg from "../views/RegisterOrg.vue";
+import OrgCard from "../components/OrgCard.vue";
+import Projekt from "../views/Projekt.vue";
+import Exitus from "../views/Exitus.vue";
 
-
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: "/",
+    name: "Home",
+    component: Home,
   },
   {
-    path: '/test',
-    name: 'Test',
-    component: Test
+    path: "/test",
+    name: "Test",
+    component: Test,
   },
   {
-    path: '/vysledek',
-    name: 'Vysledek',
-    component: Vysledek
+    path: "/vysledek",
+    name: "Vysledek",
+    component: Vysledek,
   },
   {
-    path: '/organizace',
-    name: 'Organizace',
-    component: Organizace
+    path: "/organizace",
+    name: "Organizace",
+    component: Organizace,
   },
   {
-    path: '/organizace/:id',
-    name: 'Organizace',
-    component: OrgCard
+    path: "/registrace-organizace",
+    name: "Registrace organizace",
+    component: RegisterOrg,
   },
   {
-    path: '/registrace-organizace',
-    name: 'Registrace organizace',
-    component: RegisterOrg
+    path: "/projekt",
+    name: "Projekt",
+    component: Projekt,
   },
   {
-    path: '/projekt',
-    name: 'Projekt',
-    component: Projekt
+    path: "/dekujeme",
+    name: "Dekujeme",
+    component: Exitus,
   },
-  {
-    path: '/dekujeme',
-    name: 'Dekujeme',
-    component: Exitus
-  },
-
-
-]
+];
 
 const router = new VueRouter({
-  mode: 'history',
+  mode: "history",
   base: process.env.BASE_URL,
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,8 @@
-import Vue from 'vue';
-import Vuex from 'vuex';
+import Vue from "vue";
+import Vuex from "vuex";
 Vue.use(Vuex);
 
-const URL_BASE = "https://srdcovka-db.herokuapp.com"
+const URL_BASE = "https://srdcovka-db.herokuapp.com";
 
 export const Actions = Object.freeze({
   FETCH_ORGANISATIONS: "fetchOrganisations",
@@ -11,8 +11,8 @@ export const Actions = Object.freeze({
   PERFORME_NEXT_QUESTION: "performeNextQuestion",
   PERFORM_FILE_UPLOAD: "performFileUpload",
   PERFORM_ADD_ORGANISATION: "performAddOrganisation",
-  PERFORM_RESULT_IDS: "performResultIds"
-})
+  PERFORM_RESULT_IDS: "performResultIds",
+});
 
 export const Mutations = Object.freeze({
   SET_ORGANISATIONS: "setOrganisations",
@@ -24,8 +24,7 @@ export const Mutations = Object.freeze({
   SET_RESULT: "setResult",
   SET_RESULT_IDS: "setResultIds",
   SET_RESULT_PRICE: "setResultPrice",
-
-})
+});
 
 export const Getters = Object.freeze({
   GET_ORGANISATIONS: "getOrganisations",
@@ -37,13 +36,13 @@ export const Getters = Object.freeze({
   GET_ACTUAL_QUESTION_ID: "getActualQuestionId",
   GET_RESULT_IDS: "getResultIds",
   GET_RESULT_PRICE: "getResultPrice",
-})
+});
 
 const store = new Vuex.Store({
   state: {
     organisations: [],
     result: [],
-    resultPrice: '',
+    resultPrice: "",
     resultIds: undefined,
     segments: [],
     questions: [],
@@ -52,6 +51,16 @@ const store = new Vuex.Store({
     actualQuestionId: undefined,
   },
   getters: {
+    /**
+     * Definice pomoci hrantých závorek je generické pojmenování klíče.
+     * Tzn.
+     *
+     *  getOrganisations(){
+     *    ...
+     *  }
+     *
+     * je shodné se zápisem níže. Jen používáme našeptání z výše freeznutého objektu Getters.
+     */
     [Getters.GET_ORGANISATIONS](state) {
       return state.organisations;
     },
@@ -75,9 +84,17 @@ const store = new Vuex.Store({
     },
     [Getters.GET_RESULT_IDS](state) {
       return state.resultIds;
-    }
+    },
   },
   mutations: {
+    /**
+     * Mutace mění state. Vždy mají dva parametry.
+     *
+     * První parametr - state aplikace
+     * Druhý parametr - payload (data, které chceme do statu vložit)
+     *
+     * Většinou se payload pojmenován v definici mutace tak, aby referoval s jeho obsahem.
+     */
     [Mutations.SET_ORGANISATIONS](state, organisations) {
       state.organisations = organisations;
     },
@@ -88,32 +105,37 @@ const store = new Vuex.Store({
       state.questions = questions;
     },
     [Mutations.SET_SELECTED_ORGANISATION](state, selectedOrganisationId) {
-      state.selectedOrganisation = state.organisations.find(org => org.id === selectedOrganisationId);
+      state.selectedOrganisation = state.organisations.find(
+        (org) => org.id === selectedOrganisationId
+      );
     },
     [Mutations.SET_ACTUAL_QUESTION](state, actualQuestionId) {
-      state.actualQuestion = state.questions.find(question => question.id === actualQuestionId);
+      state.actualQuestion = state.questions.find(
+        (question) => question.id === actualQuestionId
+      );
     },
     [Mutations.SET_ACTUAL_QUESTION_ID](state, actualQuestionId) {
       state.actualQuestionId = actualQuestionId;
     },
     [Mutations.SET_RESULT](state, result) {
-      state.result.push(result)
+      state.result.push(result);
     },
     [Mutations.SET_RESULT_IDS](state, resultIds) {
-      console.log("Nastavuji Result ids na:", resultIds)
-      state.resultIds = resultIds
+      state.resultIds = resultIds;
     },
     [Mutations.SET_RESULT_PRICE](state, price) {
-      console.log("Nastavuji result prize na:", price)
-      state.resultPrice = price
+      state.resultPrice = price;
     },
   },
+  /**
+   * Akce narozdíl od mutací mohou být asynchronní.
+   * Veškeré stahování dat by se mělo odehrávat v akcích storu.
+   */
   actions: {
     [Actions.FETCH_ORGANISATIONS]({ commit }) {
-
       fetch(`${URL_BASE}/organisations?validated=true`)
-        .then(response => response.json())
-        .then(organisations => {
+        .then((response) => response.json())
+        .then((organisations) => {
           const sortedOrganisations = organisations.sort((a, b) => {
             let x = a.title.toLowerCase();
             let y = b.title.toLowerCase();
@@ -125,111 +147,128 @@ const store = new Vuex.Store({
             }
             return 0;
           });
-          commit(Mutations.SET_ORGANISATIONS, sortedOrganisations)
-          commit(Mutations.SET_SELECTED_ORGANISATION, 8)
-        })
+          commit(Mutations.SET_ORGANISATIONS, sortedOrganisations);
+          commit(Mutations.SET_SELECTED_ORGANISATION, 8);
+        });
     },
 
     [Actions.FETCH_SEGMENTS]({ commit }) {
-
       fetch(`${URL_BASE}/segments`)
-        .then(response => response.json())
-        .then(segments => {
-          commit(Mutations.SET_SEGMENTS, segments)
-        })
+        .then((response) => response.json())
+        .then((segments) => {
+          commit(Mutations.SET_SEGMENTS, segments);
+        });
     },
 
     [Actions.FETCH_QUESTIONS]({ commit }) {
-
       fetch(`${URL_BASE}/questions`)
-        .then(response => response.json())
-        .then(questions => {
-          const sortedQuestions = questions.sort((a, b) => a.id - b.id)
-          const completeQuestions = sortedQuestions.map(question => {
+        .then((response) => response.json())
+        .then((questions) => {
+          const sortedQuestions = questions.sort((a, b) => a.id - b.id);
+          /**
+           * https://medium.com/coding-at-dawn/how-to-use-the-spread-operator-in-javascript-b9e4a8b06fab
+           */
+          const completeQuestions = sortedQuestions.map((question) => {
             return {
               ...question,
-              answers: question.answers.map(answer => {
+              answers: question.answers.map((answer) => {
                 return {
                   ...answer,
                   check: false,
-                }
-              })
-            }
+                };
+              }),
+            };
+          });
+          commit(Mutations.SET_QUESTIONS, completeQuestions);
 
-          })
-          commit(Mutations.SET_QUESTIONS, completeQuestions)
-
-          commit(Mutations.SET_ACTUAL_QUESTION_ID, completeQuestions[0].id)
-          commit(Mutations.SET_ACTUAL_QUESTION, completeQuestions[0].id)
-          // commit(Mutations.SET_ACTUAL_QUESTION, 61)
-        })
+          if (completeQuestions.length) {
+            commit(Mutations.SET_ACTUAL_QUESTION_ID, completeQuestions[0].id);
+            commit(Mutations.SET_ACTUAL_QUESTION, completeQuestions[0].id);
+          }
+        });
     },
-
-    [Actions.PERFORME_NEXT_QUESTION]({ commit, state }, payload) {
-      const indexActualQuestion = state.questions.findIndex(question => question.id === payload)
-      commit(Mutations.SET_ACTUAL_QUESTION_ID, state.questions[indexActualQuestion + 1].id)
-      commit(Mutations.SET_ACTUAL_QUESTION, state.questions[indexActualQuestion + 1].id)
-
+    /**
+     * let { commit, state } = context;
+     * https://javascript.info/destructuring-assignment
+     */
+    [Actions.PERFORME_NEXT_QUESTION]({ commit, state }, actualQuestionId) {
+      const indexActualQuestion = state.questions.findIndex(
+        (question) => question.id === actualQuestionId
+      );
+      commit(
+        Mutations.SET_ACTUAL_QUESTION_ID,
+        state.questions[indexActualQuestion + 1].id
+      );
+      commit(
+        Mutations.SET_ACTUAL_QUESTION,
+        state.questions[indexActualQuestion + 1].id
+      );
     },
 
     async [Actions.PERFORM_FILE_UPLOAD](context, payload) {
-      console.log("FILE:", payload)
+      console.log("FILE:", payload);
       const response = await fetch(`${URL_BASE}/upload`, {
-        method: 'POST',
-        body: payload
-      })
+        method: "POST",
+        body: payload,
+      });
 
-      const img = await response.json()
+      const img = await response.json();
+
+      /**
+       * Z této akce vrací ID uploadnutého obrázku pro udání refernce na obrázek v databázi.
+       * STRAPI si tuto referenci řeší sám, jen potřebuje mít nastavené ID obrázku, se kterým
+       * se chce provázat.
+       */
 
       if (img.length && img[0].id) {
-        return img[0].id
+        return img[0].id;
       }
     },
 
     [Actions.PERFORM_ADD_ORGANISATION](context, payload) {
-
-      console.log("Payload:", payload)
-      const segmentId = context.state.segments.find(segment => segment.name === payload.segment)
-      console.log("Hledám ID segmentu:", segmentId)
+      console.log("Payload:", payload);
+      const segmentId = context.state.segments.find(
+        (segment) => segment.name === payload.segment
+      );
 
       const data = {
         ...payload,
-        segment: segmentId.id
-      }
+        segment: segmentId.id,
+      };
 
-      console.log("Snažím se o změnu segmentu na ID:", data);
       fetch(`${URL_BASE}/organisations`, {
-        method: 'POST',
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json'
+          "Content-Type": "application/json",
         },
-        body: JSON.stringify(data)
-      })
-        .then(response => response.json())
-        .then(organisation => console.log(organisation));
+        body: JSON.stringify(data),
+      }).catch((error) => console.log(error));
     },
     [Actions.PERFORM_RESULT_IDS]({ commit, state }) {
-      const resultCategories = state.result
+      const resultCategories = state.result;
       let map = resultCategories.reduce((p, c) => {
         p[c] = (p[c] || 0) + 1;
         return p;
-      }, {})
+      }, {});
 
-      const resultObject = Object.keys(map).map(item => {
-        return {
-          category: item,
-          count: map[item],
-          id: state.segments.find(segment => segment.key === item).id,
-          icon: state.segments.find(segment => segment.key === item).icon,
-          name: state.segments.find(segment => segment.key === item).name,
-          description: state.segments.find(segment => segment.key === item).description
-        }
-      }).sort((a, b) => b.count - a.count).slice(0, 2)
+      const resultObject = Object.keys(map)
+        .map((item) => {
+          return {
+            category: item,
+            count: map[item],
+            id: state.segments.find((segment) => segment.key === item).id,
+            icon: state.segments.find((segment) => segment.key === item).icon,
+            name: state.segments.find((segment) => segment.key === item).name,
+            description: state.segments.find((segment) => segment.key === item)
+              .description,
+          };
+        })
+        .sort((a, b) => b.count - a.count)
+        .slice(0, 2);
 
-      commit(Mutations.SET_RESULT_IDS, resultObject)
+      commit(Mutations.SET_RESULT_IDS, resultObject);
     },
-
-  }
+  },
 });
 
 export default store;
